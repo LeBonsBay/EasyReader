1087515f0617d9023e2953ce88f01b8002a2ce1c
LeBonsBay <sasuek94@gmail.com>
2025-05-23 09:49:21 +0200

    what the fuck am I doinf

--- app/src/main/java/com/example/easyreader/SettingsActivity.java
+++ app/src/main/java/com/example/easyreader/SettingsActivity.java
@@ -1,10 +1,6 @@
 package com.example.easyreader;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,46 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class MainActivity extends AppCompatActivity {
-
-
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //
-
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_settings);
-
-
-
-
-        //
-
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-
-
     }
-
-    public void onClickSettings(){
-
-
-        //OnClickListener
-
-
-
-
-    }
-
 }
