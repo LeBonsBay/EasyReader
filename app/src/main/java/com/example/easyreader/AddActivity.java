2ad297fa2de092aa71e90ee3f430f18d82b5165b
LeBonsBay <sasuek94@gmail.com>
2025-05-29 23:45:59 +0200

    added navigation through scenes/activites next is going to be the "add" section --> pdf files?

--- app/src/main/java/com/example/easyreader/AddActivity.java
+++ app/src/main/java/com/example/easyreader/AddActivity.java
@@ -2,7 +2,6 @@ package com.example.easyreader;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,37 +11,43 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class SettingsActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_settings), (v, insets) -> {
+        setContentView(R.layout.activity_add);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_add), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+        bottomNavigationView.setOnItemSelectedListener(item -> { //lambda ist verkürzte schreibweise von diesem komischen code da wenn du auf lambda gehst; machen wir noch genauer
             if (item.getItemId() == R.id.nav_add) {
 
-                Intent in = new Intent(getApplicationContext(), AddActivity.class);
-                this.startActivity(in);
-                return true;
+                return false;
+
             } else if (item.getItemId() == R.id.nav_home) {
-                Intent in = new Intent(this.getApplicationContext(),MainActivity.class);
+                Intent in = new Intent(this.getApplicationContext(), MainActivity.class);
+                //hier wird der Befehl ausgeführt, dass das Fenster der SettingsActivity geöffnet wird und bei der Class ist durch setContentView ja das xml schon festgelegt, deshalb nicht hier extra machen
                 this.startActivity(in);
+
                 return true;
             } else if (item.getItemId() == R.id.nav_settings) {
+                Intent in = new Intent(this.getApplicationContext(), SettingsActivity.class);
+                //hier wird der Befehl ausgeführt, dass das Fenster der SettingsActivity geöffnet wird und bei der Class ist durch setContentView ja das xml schon festgelegt, deshalb nicht hier extra machen
+                this.startActivity(in);
 
-                return false;
+                return true;
             }
             return false;
 
         });
+
+
     }
 }
--- app/src/main/java/com/example/easyreader/MainActivity.java
+++ app/src/main/java/com/example/easyreader/MainActivity.java
@@ -2,54 +2,71 @@ package com.example.easyreader;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //
-
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_settings);
-
-
-
-
-        //
-
         super.onCreate(savedInstanceState);
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
         });
+        //1. von hausaus immer unter einem super den code pasten.
+        //2. das bottom_nav_menu.xml tue ich bereits mit einer id ja bei der activity_main.xml ja verlinken, siehe code bei der bottomnavigationview
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        bottomNavigationView.setOnItemSelectedListener(item -> { //lambda ist verkürzte schreibweise von diesem komischen code da wenn du auf lambda gehst; machen wir noch genauer
+            if (item.getItemId() == R.id.nav_add) {
+                Intent in = new Intent(this.getApplicationContext(), AddActivity.class);
+                //hier wird der Befehl ausgeführt, dass das Fenster der SettingsActivity geöffnet wird und bei der Class ist durch setContentView ja das xml schon festgelegt, deshalb nicht hier extra machen
+                this.startActivity(in);
+
+                return true;
+
+            } else if (item.getItemId() == R.id.nav_home) {
+
+                return false;
+            } else if (item.getItemId() == R.id.nav_settings) {
+                Intent in = new Intent(this.getApplicationContext(), SettingsActivity.class);
+                //hier wird der Befehl ausgeführt, dass das Fenster der SettingsActivity geöffnet wird und bei der Class ist durch setContentView ja das xml schon festgelegt, deshalb nicht hier extra machen
+                this.startActivity(in);
+
+                return true;
+            }
+            return false;
+
+        });
     }
 
-    public void onClickSettings(){
+    public void onClickSettings() {
 
 
         //OnClickListener
-
-
 
 
     }
